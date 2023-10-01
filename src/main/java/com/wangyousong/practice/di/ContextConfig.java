1c29335490d6497effefd0c19bebbc631a8b7e55
Bob Jackson <wangyousong@9191offer.com>
2023-10-01 12:32:51 +0800

    refactor: ComponentProvider 替代原始的Provider,这样context不是每次都是new出来的

--- src/main/java/com/wangyousong/practice/di/ContextConfig.java
+++ src/main/java/com/wangyousong/practice/di/ContextConfig.java
@@ -1,7 +1,6 @@
 package com.wangyousong.practice.di;
 
 import jakarta.inject.Inject;
-import jakarta.inject.Provider;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
@@ -13,16 +12,15 @@ import java.util.Optional;
 import static java.util.Arrays.stream;
 
 public class ContextConfig {
-    private final Map<Class<?>, Provider<?>> providers = new HashMap<>();
+    private final Map<Class<?>, ComponentProvider<?>> providers = new HashMap<>();
 
     public <Type> void bind(Class<Type> type, Type instance) {
-        providers.put(type, (Provider<Type>) () -> instance);
+        providers.put(type, context -> instance);
     }
 
     public <Type, Implementation extends Type>
     void bind(Class<Type> type, Class<Implementation> implementation) {
         Constructor<Implementation> injectConstructor = getInjectConstructor(implementation);
-
         providers.put(type, new ConstructorInjectionProvider<>(type, injectConstructor));
     }
 
@@ -30,7 +28,7 @@ public class ContextConfig {
         return new Context() {
             @Override
             public <Type> Optional<Type> get(Class<Type> type) {
-                return Optional.ofNullable(providers.get(type)).map(provider -> (Type) provider.get());
+                return Optional.ofNullable(providers.get(type)).map(provider -> (Type) provider.get(this));
             }
         };
     }
@@ -51,9 +49,13 @@ public class ContextConfig {
         });
     }
 
-    class ConstructorInjectionProvider<T> implements Provider<T> {
-        private Class<?> componentType;
-        private Constructor<T> injectConstructor;
+    interface ComponentProvider<T> {
+        T get(Context context);
+    }
+
+    static class ConstructorInjectionProvider<T> implements ComponentProvider<T> {
+        private final Class<?> componentType;
+        private final Constructor<T> injectConstructor;
         private boolean constructing;
 
         public ConstructorInjectionProvider(Class<?> componentType, Constructor<T> injectConstructor) {
@@ -62,12 +64,12 @@ public class ContextConfig {
         }
 
         @Override
-        public T get() {
+        public T get(Context context) {
             try {
                 if (constructing) throw new CyclicDependenciesFoundException(componentType);
                 constructing = true;
                 Object[] dependencies = stream(injectConstructor.getParameters())
-                        .map(p -> getContext().get(p.getType())
+                        .map(p -> context.get(p.getType())
                                 .orElseThrow(() -> new DependencyNotFoundException(componentType, p.getType())))
                         .toArray(Object[]::new);
                 return injectConstructor.newInstance(dependencies);
@@ -79,6 +81,5 @@ public class ContextConfig {
                 constructing = false;
             }
         }
-
     }
 }
