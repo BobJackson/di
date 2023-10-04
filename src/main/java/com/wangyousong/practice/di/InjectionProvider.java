926558ef3715b8ed39298639914ee2b03839faba
Bob Jackson <wangyousong@9191offer.com>
2023-10-04 20:24:11 +0800

    refactor: extract toDependency()

--- src/main/java/com/wangyousong/practice/di/InjectionProvider.java
+++ src/main/java/com/wangyousong/practice/di/InjectionProvider.java
@@ -125,16 +125,16 @@ class InjectionProvider<T> implements ContextConfig.ComponentProvider<T> {
     }
 
     private static Object[] toDependencies(Context context, Executable executable) {
-        return stream(executable.getParameters()).map(
-                p -> {
-                    Type type = p.getParameterizedType();
-                    if (type instanceof ParameterizedType) return context.get((ParameterizedType) type).get();
-                    return context.get((Class<?>) type).get();
-                }).toArray(Object[]::new);
+        return stream(executable.getParameters())
+                .map(p -> toDependency(context, p.getParameterizedType()))
+                .toArray(Object[]::new);
     }
 
     private static Object toDependency(Context context, Field field) {
-        Type type = field.getGenericType();
+        return toDependency(context, field.getGenericType());
+    }
+
+    private static Object toDependency(Context context, Type type) {
         if (type instanceof ParameterizedType) return context.get((ParameterizedType) type).get();
         return context.get((Class<?>) type).get();
     }
